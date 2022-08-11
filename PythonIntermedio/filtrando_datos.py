7dd34497024d64b561a02f8a9162c8eb22c07c10
PaolaAndreaGA <paga0209@gmial.com>
2022-08-11 17:02:17 -0500

    ejercicios python intermedio

---
+++ PythonIntermedio/filtrando_datos.py
@@ -0,0 +1,99 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+
+
+
+def run():
+  all_js_devs = [worker["name"] for worker in DATA if worker["language"] == "javascript"]
+  all_adults = list(filter(lambda x: x["age"] > 17, DATA))
+  all_adults = list(map(lambda y: y["name"], all_adults))
+  old_people = list(map(lambda worker: worker| {"old": worker["age"]>70}, DATA))
+  print("programadores de javascript")
+  for worker in all_js_devs:
+    print(worker)
+
+  print("\n")
+  print("programadores mayores de edad")
+  for worker in all_adults:
+    print(worker)
+
+  print("\n")
+  print("programadores veteranos")
+  for worker in old_people:
+    if worker["old"] == True:
+      print(worker["name"])
+
+if __name__ == '__main__':
+  run()
